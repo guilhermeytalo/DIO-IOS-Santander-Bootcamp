2a29e487092c5d4a5ba3b505cd0defff68b905a6
Guilherme Ytalo <gydaytalo@gmail.com>
2023-09-02 00:56:34 -0300

    fix: using xcode now

---
+++ variables_and_constants.swiftpm/lesson1.swift
@@ -0,0 +1,26 @@
+func main() {
+    // Declarating constants and variables
+    let maximumNumberOfLoginAttemps = 10
+    var _currentLoginAttempts = 0
+
+    // Type notations
+    var welcomeMessage: String
+    welcomeMessage = "Hello"
+    
+    // Nomenclature
+    let `public` = "General"
+    let pi = 3.14159
+    let algo = "Algo"
+    let 🐶🐮 = "dogcow"
+    var friendlyWelcome = "Hello!"
+    friendlyWelcome = "Bonjour"
+    
+    let languageName = "swift"
+    // language = "Swift++" // Not permited
+    
+    
+    // printingValues
+    print(friendlyWelcome, terminator: "")
+    print(friendlyWelcome)
+    print("O valor atual de friendlyWelcome é \(friendlyWelcome)")
+}
